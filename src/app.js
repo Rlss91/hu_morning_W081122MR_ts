9b74f9ff25e5ea1e911c6347001417c1646d42ec
Rlss91 <rlss91@gmail.com>
2023-02-07 11:08:58 +0200

    cb functions

--- src/app.js
+++ src/app.js
@@ -1,41 +1,15 @@
-"use strict";
-let literally = 10;
-let number;
-let str;
-let bool;
-let anyVal;
-const calc = (a, b, c) => {
-    switch (c) {
-        case "-":
-            return a - b;
-        case "*":
-            return a * b;
-        case "/":
-            return a / b;
-        case "+":
-        default:
-            return a + b;
-    }
+var UserType;
+(function (UserType) {
+    UserType[UserType["NORMAL"] = 1] = "NORMAL";
+    UserType[UserType["BUSINESS"] = 2] = "BUSINESS";
+    UserType[UserType["VIP"] = 3] = "VIP";
+    UserType[UserType["ADMIN"] = 4] = "ADMIN";
+})(UserType || (UserType = {}));
+const f1 = () => { };
+const f2 = () => { };
+const callF1 = (functionAsParam) => {
+    functionAsParam();
 };
-console.log(calc(50, 50, "aaaaaaaaaaaaaaaaaa"));
-anyVal = 5;
-anyVal = "str";
-anyVal = {};
-let unionTyp;
-unionTyp = 5;
-unionTyp = "hello world from ts";
-let user;
-user = {
-    name: "kenny",
-    age: 8,
-};
-let user2;
-user2 = {
-    name: "kenny",
-    age: 8,
-};
-let user3;
-let user4;
-user4 = {
-    name: "ke",
-};
+callF1(f1);
+callF1(f2);
+export {};
--- ts/app.ts
+++ ts/app.ts
@@ -1,58 +1,103 @@
-let literally = 10;
-let number: number;
-let str: string;
-let bool: boolean;
-let anyVal: any;
+// let literally = 10;
+// let number: number;
+// let str: string;
+// let bool: boolean;
+// let anyVal: any;
 
-const calc = (a: number, b: number, c: string): number => {
-  switch (c) {
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    case "+":
-    default:
-      return a + b;
-  }
-};
+// const calc = (a: number, b: number, c: string): number => {
+//   switch (c) {
+//     case "-":
+//       return a - b;
+//     case "*":
+//       return a * b;
+//     case "/":
+//       return a / b;
+//     case "+":
+//     default:
+//       return a + b;
+//   }
+// };
 
-console.log(calc(50, 50, "aaaaaaaaaaaaaaaaaa"));
+// console.log(calc(50, 50, "aaaaaaaaaaaaaaaaaa"));
 
-anyVal = 5;
-anyVal = "str";
-anyVal = {};
+// anyVal = 5;
+// anyVal = "str";
+// anyVal = {};
 
-let unionTyp: number | string | boolean;
-unionTyp = 5;
-unionTyp = "hello world from ts";
+// let unionTyp: number | string | boolean;
+// unionTyp = 5;
+// unionTyp = "hello world from ts";
 
-// let obj: object;
-// obj = { name: "kenny", age: 8 };
-// obj.name;
+// // let obj: object;
+// // obj = { name: "kenny", age: 8 };
+// // obj.name;
 
-let user: { name: string; age: number };
-user = {
-  name: "kenny",
-  age: 8,
-};
+// let user: { name: string; age: number };
+// user = {
+//   name: "kenny",
+//   age: 8,
+// };
 
-let user2: { name: string; age: number; gender?: boolean };
+// let user2: { name: string; age: number; gender?: boolean };
 
-user2 = {
-  name: "kenny",
-  age: 8,
-};
+// user2 = {
+//   name: "kenny",
+//   age: 8,
+// };
+
+// interface User {
+//   name: string;
+//   age: number;
+//   gender?: boolean;
+// }
+
+// let user3: User;
+// let user4: User;
+// user4 = {
+//   name: "ke",
+// };
+
+// let arr: Array<User | string> = [{ name: "kenny", age: 8 }];
+// console.log(arr);
+
+// let arr2: string[];
+// let arr3: User[];
+// let arr4: (User | string)[];
+
+enum UserType {
+  NORMAL = 1,
+  BUSINESS,
+  VIP,
+  ADMIN,
+}
 
 interface User {
   name: string;
   age: number;
-  gender?: boolean;
+  type: UserType;
 }
 
-let user3: User;
-let user4: User;
-user4 = {
-  name: "ke",
+// let user: User = {
+//   name: "kenny",
+//   age: 8,
+//   type: UserType.NORMAL,
+// };
+
+// const genericFunction = <T>(a: T): T => {
+//   console.log(a);
+//   return a;
+// };
+
+// genericFunction<number>(5);
+// genericFunction<User>();
+
+const f1 = () => {};
+const f2 = () => {};
+const callF1 = (functionAsParam: Function) => {
+  functionAsParam(); // f1()
 };
+
+callF1(f1);
+callF1(f2);
+
+export {};
